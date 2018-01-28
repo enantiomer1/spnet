bc77b786e1abfd3366083672afad3aa20de79d3b
enantiomer1 <jecm@protonmail.com>
2018-01-28 17:43:20 -0600

    small edits

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -10,7 +10,8 @@
                 <div class="card-body">
                     <p>Sponsor-Net.org was created as a service work project to give back what the program has freely given to us.</p><hr>
                     <p>This site is not associated with Alcoholics Anonymous World Services, Inc., but fully supports it and its services.</p><hr>
-                    <p>Sponsor-net.org is a non-porfit organization, Please donate to offset hosting web hosting operational costs if possible, thank you!</p>  
+                    <p>Sponsor-net.org is a non-porfit organization, Please donate to offset web hosting operational costs if possible, thank you!</p><hr>
+                    <p>We use an internally stored zipcode database for privacy and all user information is always kept confidential.</p> 
                 </div>
             </div><!--card--> 
         </div><!--col-->
